959744bc9ce564a5e2a3d0ee64bce0566f05db3a
saikrishnachinthala7-ui <saikrishnachinthala7@gmail.com>
2026-05-14 00:40:13 +0530

    delete

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-
-export const supabase = createClient(
-  'https://ehsqnfmctdosebfcakwv.supabase.co',
-  'sb_publishable_r85tUWIcXp-tRJ7OypsXWw_Oy4ijkfh'
-);
